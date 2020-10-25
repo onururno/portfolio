fdf65c40ba3caed39336185b4b5ca7260ab6fb31
Onur <onur-erbil@hotmail.de>
2020-10-25 14:25:46 +0100

    Added Burger-menu and fixed hero intro mobile version

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,3 +7,24 @@ if (module.hot) {
 }
 
 
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
+  burger.classList.toggle("burgerOut");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+    burger.classList.toggle("burgerOut");
+  });
+});
+
+AOS.init({
+  duration: 1200,
+});
