75f1afa08dab70a20c175e0fb61497963107f4a1
Onur <onur-erbil@hotmail.de>
2020-12-26 12:19:53 +0100

    stop burger animation when you press links

--- app/assets/scripts/modules/burger.js
+++ app/assets/scripts/modules/burger.js
@@ -1,7 +1,8 @@
 const Burger = () => {
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
-const links = nav.querySelectorAll("a");
+const link = document.querySelector(".burger-work");
+// const links = nav.querySelectorAll("a");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
@@ -9,13 +10,11 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("burgerOut");
 });
 
-links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
     burger.classList.toggle("burgerOut");
   });
-});
 
 AOS.init({
   duration: 1200,
